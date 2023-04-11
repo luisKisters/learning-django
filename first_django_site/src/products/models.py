7cef69af99b83c180980a54078291811de0a5c7c
luisKisters <luis.w.kisters@gmail.com>
2023-04-11 16:14:35 +0200

    completed "Render Data from the Database with a Model"

--- first_django_site/src/products/models.py
+++ first_django_site/src/products/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Product(models.Model):
     title       = models.CharField(max_length=120) # max_length is required
     description = models.TextField(blank=True, null=True)
-    price       = models.DecimalField(decimal_places=2, max_digits=1000)
+    price       = models.TextField()
     summary     = models.TextField(blank=True, null=False) # If blank is set to True, it makes it so that the field is required
     feature     = models.BooleanField()
